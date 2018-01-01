1de65b3095cc9dc56eebaaa32e36ff177888d108
Luciano Cesar <lucianocordeiro1991@hotmail.com>
2018-01-01 15:35:45 -0200

    MNG

---
+++ megasena_number_generator.rb
@@ -0,0 +1,12 @@
+def self.mg
+  x = 1.step(60, 1).to_a
+  y = x.sample(6).sort
+  print "Seus números da megasena são: "
+  for z in y
+    if z < 10
+      print "0" + z.to_s + " "
+    else
+      print z.to_s + " "
+    end
+  end
+end
