eca9512c07af5083b501fa2d7821852747004a14
LucianoCordeiro <32871795+LucianoCordeiro@users.noreply.github.com>
2018-01-01 23:50:59 -0200

    Update megasena_number_generator.rb

--- megasena_number_generator.rb
+++ megasena_number_generator.rb
@@ -1,12 +1,13 @@
-def megasena_number_generator
-  x = 1.step(60, 1).to_a
-  y = x.sample(6).sort
-  print "Seus números da megasena são: "
-  for z in y
-    if z < 10
-      print "0" + z.to_s + " "
-    else
-      print z.to_s + " "
-    end
+#This is the megasena lottery number generator!!
+
+x = 1.step(60, 1).to_a 
+y = x.sample(6).sort
+print "Seus números da megasena são: "
+for z in y
+  if z < 10
+    print "0" + z.to_s + " "
+  else
+    print z.to_s + " "
   end
 end
+
