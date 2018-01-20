d4836be672ed982927ea8c3d43b6a9e9e1076046
LucianoCordeiro <32871795+LucianoCordeiro@users.noreply.github.com>
2018-01-20 21:50:18 -0200

    Create class_cat.rb

---
+++ class_cat.rb
@@ -0,0 +1,37 @@
+class Cat 
+  
+  attr_accessor :id, :name, :age
+  
+  def initialize(id = nil, name = nil, age = nil)
+    @id = id
+    @name = name
+    @age = age
+  end
+  
+  @cats ||= []
+  
+  def self.create(name, age)
+    @cats << Cat.new(@cats.size + 1, name, age)
+  end
+  
+  def self.find(cat_id)
+    @cats[cat_id - 1]
+  end
+  
+  def self.count 
+    @cats.size
+  end
+  
+  def self.all 
+    @cats
+  end
+
+end
+
+Cat.create("Neto", 13)
+Cat.create("Mario", 6)
+Cat.create("Gabriel", 16)
+Cat.create("Anderson", 7)
+
+Cat.count # => 4
+Cat.find(3) # =>  #<Cat:0x0055a34d04a538 @id=3, @name="Gabriel", @age=16>
