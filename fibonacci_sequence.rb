bee9054804a6f7fc0b733558333a066dde38eb64
LucianoCordeiro <32871795+LucianoCordeiro@users.noreply.github.com>
2018-01-26 17:15:31 -0200

    Create fibonacci_sequence.rb

---
+++ fibonacci_sequence.rb
@@ -0,0 +1,11 @@
+def fibonacci_sequence(array_size)
+  y = [0,1]
+  i = 1 
+  while i < array_size - 1
+    y << y[y.length - 2] + y[y.length - 1]
+    i += 1
+  end
+  return y
+end
+
+fibonnaci_sequence(9) # => [0, 1, 1, 2, 3, 5, 8, 13, 21]
