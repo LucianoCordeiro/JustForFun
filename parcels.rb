6e18782b57e758452706966aeec6c2a4eacc8e89
Luciano Cesar Cordeiro <32871795+LucianoCordeiro@users.noreply.github.com>
2018-02-11 02:06:35 -0200

    Update parcels.rb

--- parcels.rb
+++ parcels.rb
@@ -3,14 +3,15 @@ def parcel(sum, num_of_parcels)
   parcels_array = []
   
   division = sum.to_f / num_of_parcels
-  array_division = division.to_s.split * num_of_parcels
-  array_division.each do |parcel|
-    parcels_array << parcel[0..3].to_f
+  
+  i = 0
+  while i < num_of_parcels
+    parcels_array[i] = division.to_s[0..3].to_f
+    i += 1
   end
   
   parcels_array
   
 end 
-
 
 parcel(22, 7) # => [3.14, 3.14, 3.14, 3.14, 3.14, 3.14, 3.14]
