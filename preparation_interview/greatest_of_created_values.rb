f6aec12b2845b92e1f48f60439aa9c6b1317a7e5
Luciano Cesar Cordeiro <32871795+LucianoCordeiro@users.noreply.github.com>
2018-02-24 23:50:54 -0300

    Update greatest_of_created_values.rb

--- preparation_interview/greatest_of_created_values.rb
+++ preparation_interview/greatest_of_created_values.rb
@@ -1,12 +1,12 @@
 class Value_Collection
   
-  @@arr = []
+  @arr = []
   
-  def greatest_value(num)
+  def self.greatest_value(num)
   
-    @@arr << num
-    valmax = @@arr[0]
-    @@arr.each {|a| valmax = a if valmax < a}
+    @arr << num
+    valmax = @arr[0]
+    @arr.each {|a| valmax = a if valmax < a}
     return valmax
     
   end
