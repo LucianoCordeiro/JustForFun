c8753782315b8e183e1b7691cf7b138932b3aeac
Luciano Cesar Cordeiro <32871795+LucianoCordeiro@users.noreply.github.com>
2018-03-17 10:58:52 -0300

    Create bubble_method.rb

---
+++ sorting_methods/bubble_method.rb
@@ -0,0 +1,14 @@
+def bubble_sort(array)
+  n = array.size - 1
+  n.times do
+    swapped = false
+    n.times do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = true
+      end
+    end
+    break if swapped == false
+  end
+  array
+end
