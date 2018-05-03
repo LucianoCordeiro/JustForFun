4c584dcbc0d05da0b5c2c35f3e0b84bce08ec2af
Luciano Cesar Cordeiro <32871795+LucianoCordeiro@users.noreply.github.com>
2018-05-03 13:48:55 -0300

    Update current_second_greatest_absolute_value.rb

--- preparation_interview/current_second_greatest_absolute_value.rb
+++ preparation_interview/current_second_greatest_absolute_value.rb
@@ -11,8 +11,8 @@ class Value_Collection
 
     exval = [] << @arr[0] << @arr[1] << @arr[-2] << @arr[-1]
 
-    exval[0] + -2 * exval[0] > exval[-1] ? exval.shift : exval.pop
-    exval[0] + -2 * exval[0] > exval[-1] ? exval.shift : exval.pop
+    -exval[0] > exval[-1] ? exval.shift : exval.pop
+    -exval[0] > exval[-1] ? exval.shift : exval.pop
     
   end
 end 
