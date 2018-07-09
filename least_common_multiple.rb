028653c2f74513fbad0e774906cd949b2b7aaac9
Luciano Cesar Cordeiro <32871795+LucianoCordeiro@users.noreply.github.com>
2018-07-08 21:18:49 -0300

    Update least_common_multiple.rb

--- least_common_multiple.rb
+++ least_common_multiple.rb
@@ -1,16 +1,9 @@
 def lcm(num1, num2)
-num1_multiples, num2_multiples = [], []
-
-multiplier = 1
+  num1, num2 = num2, num1 if num1 < num2
+  multiplier = 1
   loop do
-    multiple1 = num1 * multiplier
-    return multiple1 if num2_multiples.include?(multiple1)
-    num1_multiples << multiple1
-
-    multiple2 = num2 * multiplier
-    return multiple2 if num1_multiples.include?(multiple2)
-    num2_multiples << multiple2
-   
+    multiple = num1 * multiplier
+    return multiple if multiple % num2 == 0  
     multiplier += 1
   end
 end
