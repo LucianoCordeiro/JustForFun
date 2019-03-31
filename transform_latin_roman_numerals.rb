6c0558fb761531071c4c3c3803b76687162fce32
Luciano Cesar Cordeiro <32871795+LucianoCordeiro@users.noreply.github.com>
2019-03-31 09:13:38 -0300

    Update and rename latin_to_roman_numeral.rb to transform_latin_roman_numerals.rb

--- transform_latin_roman_numerals.rb
+++ transform_latin_roman_numerals.rb
@@ -22,3 +22,33 @@ def transform_to_roman(latin_numeral)
   end
   roman_numeral
 end
+
+def transform_to_latin(roman_numeral)
+
+  values = {
+    'I' => 1,
+    'V' => 5,
+    'X' => 10,
+    'L' => 50,
+    'C' => 100,
+    'D' => 500,
+    'M' => 1000
+  }
+
+  subtract_when = {
+    'I' => ['V', 'X'],
+    'X' => ['L', 'C'],
+    'C' => ['D', 'M']
+  }
+
+  i, sum = 0, 0
+  while i < roman_numeral.size
+    if subtract_when[roman_numeral[i]]&.include?(roman_numeral[i+1])
+      sum -= values[roman_numeral[i]]
+    else 
+      sum += values[roman_numeral[i]]
+    end
+    i += 1
+  end
+  sum
+end
